4068111cffa79e665a68808a41dd8fa2275c605a
Jules-mlrd <jules-mlrd@users.noreply.github.com>
2025-11-03 14:46:45 +0100

    Fix: Ajout du contenu manquant dans app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+﻿const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello ESME DevOps 2025!');
+});
+
+app.listen(PORT, () => {
+  console.log('Application demarree sur le port ' + PORT);
+});
